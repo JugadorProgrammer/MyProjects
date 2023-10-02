993f982453f536ae679e83aada71f7367bb016c2
JugadorProgrammer <dinamomoscow2004@gmail.com>
2023-10-02 19:53:03 +0300

    delete

--- Diary/Diary/DataBase/Day.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Diary.DataBase
-{
-    public class Day
-    {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public string Value { get; set; } = string.Empty;
-    }
-}
