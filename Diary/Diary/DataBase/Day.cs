fdf77e560f00a130c6156b1af2899a4a07b7a8ce
JugadorProgrammer <dinamomoscow2004@gmail.com>
2023-10-02 19:55:53 +0300

    Diary
    I wrote a diary for user convenience. The site was written in asp.net core using Entity Framwork Core and SQL Server

---
+++ Diary/Diary/DataBase/Day.cs
@@ -0,0 +1,9 @@
+﻿namespace Diary.DataBase
+{
+    public class Day
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Value { get; set; } = string.Empty;
+    }
+}
